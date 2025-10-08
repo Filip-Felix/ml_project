7ce89347dc9418ea41ad791b0ff712ef15999cfb
Filip-Felix <115398797+Filip-Felix@users.noreply.github.com>
2025-10-08 08:23:00 +0300

    Add files via upload

---
+++ Project/Assets/Scripts/FreeCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class FreeCamera : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float lookSpeed = 2f;
+
+    private float yaw = 0f;
+    private float pitch = 0f;
+
+    void Update()
+    {
+        // Mișcare
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float upDown = 0f;
+
+        if (Input.GetKey(KeyCode.E)) upDown = 1f;
+        if (Input.GetKey(KeyCode.Q)) upDown = -1f;
+
+        Vector3 move = new Vector3(h, upDown, v) * moveSpeed * Time.deltaTime;
+        transform.Translate(move);
+
+        // Rotire mouse
+        if (Input.GetMouseButton(1)) // click dreapta
+        {
+            yaw += lookSpeed * Input.GetAxis("Mouse X");
+            pitch -= lookSpeed * Input.GetAxis("Mouse Y");
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+    }
+}
